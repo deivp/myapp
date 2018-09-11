8ad5cb73dfa61ae8fcfe3268514b582643c0e8bc
David Isama <ispwebdavid@gmail.com>
2018-09-11 00:05:37 -0500

    se edito el archivo welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,10 +69,10 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Inicio</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Acceder</a>
+                        <a href="{{ route('register') }}">Registrarse</a>
                     @endauth
                 </div>
             @endif
